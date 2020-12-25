b5489af1a5b560aed8a6db69ec3b18ab2e6fca46
ekluthra <ekluthra@yahoo.com>
2020-12-25 14:15:23 -0500

    added background, hover, and active colors to navbar and links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,19 +3,36 @@ import { NavLink } from 'react-router-dom';
 
 export default function NavBar(){
     return (
-        <header>
-            <div>
-                <nav>
-                    <NavLink to="/" exact>
+        <header className="bg-gray-900 bg-opacity-60">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+                    <NavLink 
+                        to="/" 
+                        exact 
+                        activeClassName="text-red-400"
+                        className="inflex-flex items-center py-6 px-3 mr-4 text-white navfont hover:text-red-400 text-4xl font-bold tracking-widest" 
+                    >
                         Ekagra Luthra
                     </NavLink>
-                    <NavLink to="/about">
+                    <NavLink 
+                        to="/about" 
+                        activeClassName="text-red-400"
+                        className = "inline-flex items-center py-3 px-3 my-6 rounded navfont text-white hover:text-red-400"
+                    >
                         About
                     </NavLink>
-                    <NavLink to="/project">
+                    <NavLink 
+                        to="/project" 
+                        activeClassName="text-red-400"
+                        className = "inline-flex items-center py-3 px-3 my-6 rounded navfont text-white hover:text-red-400"
+                    >
                         Projects
                     </NavLink>
-                    <NavLink to="/post">
+                    <NavLink 
+                        to="/post" 
+                        activeClassName="text-red-400"
+                        className = "inline-flex items-center py-3 px-3 my-6 rounded navfont text-white hover:text-red-400"
+                    >
                         Blog
                     </NavLink>
                 </nav>
