df8c279de7ca546f500a19ba3e39b054ac4dd1a9
ekluthra <ekluthra@yahoo.com>
2020-12-27 23:59:05 -0500

    made navbar overlap other components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,41 +1,58 @@
 import React from "react"
 import { NavLink } from 'react-router-dom';
+import { SocialIcon } from 'react-social-icons';
 
 export default function NavBar(){
     return (
-        <header className="bg-gray-900 bg-opacity-60">
+        //changed here
+        <header className="bg-gray-900 bg-opacity-60 w-screen fixed z-10">
             <div className="container mx-auto flex justify-between">
                 <nav className="flex">
                     <NavLink 
                         to="/" 
                         exact 
-                        activeClassName="text-red-400 rounded bg-black bg-opacity-40"
-                        className="inflex-flex items-center py-6 px-3 mr-4 text-white navfont hover:text-red-400 text-4xl font-bold tracking-widest" 
+                        activeClassName="text-green-100 rounded bg-black bg-opacity-40"
+                        className="inflex-flex items-center py-6 px-3 mr-4 text-white navfont hover:text-green-100 text-4xl font-bold tracking-widest" 
                     >
                         Ekagra Luthra
                     </NavLink>
                     <NavLink 
                         to="/about" 
-                        activeClassName="text-red-400 rounded bg-black bg-opacity-40"
-                        className = "inline-flex items-center py-3 px-3 my-6 rounded navfont text-white hover:text-red-400"
+                        activeClassName="text-green-100 rounded bg-black bg-opacity-40"
+                        className = "inline-flex items-center py-3 px-3 my-6 rounded navfont text-white hover:text-green-100"
                     >
                         About
                     </NavLink>
                     <NavLink 
                         to="/project" 
-                        activeClassName="text-red-400 rounded bg-black bg-opacity-40"
-                        className = "inline-flex items-center py-3 px-3 my-6 rounded navfont text-white hover:text-red-400"
+                        activeClassName="text-green-100 rounded bg-black bg-opacity-40"
+                        className = "inline-flex items-center py-3 px-3 my-6 rounded navfont text-white hover:text-green-100"
                     >
                         Projects
                     </NavLink>
                     <NavLink 
                         to="/post" 
-                        activeClassName="text-red-400 rounded bg-black bg-opacity-40"
-                        className = "inline-flex items-center py-3 px-3 my-6 rounded navfont text-white hover:text-red-400"
+                        activeClassName="text-green-100 rounded bg-black bg-opacity-40"
+                        className = "inline-flex items-center py-3 px-3 my-6 rounded navfont text-white hover:text-green-100"
                     >
                         Blog
                     </NavLink>
                 </nav>
+                <div className="inline-flex py-3 px-3 my-6">
+                    <SocialIcon 
+                        url="https://www.linkedin.com/in/ekagra-luthra/" 
+                        className="mr-4" 
+                        target="_blank" 
+                        fgColor="#fff" 
+                        style={{ height: 35, width: 35}}
+                    />
+                    <SocialIcon 
+                        url="https://github.com/ekluthra" 
+                        className="mr-4" 
+                        target="_blank" 
+                        fgColor="#fff" 
+                        style={{ height: 35, width: 35}}/>
+                </div>
             </div>
         </header>
     )
