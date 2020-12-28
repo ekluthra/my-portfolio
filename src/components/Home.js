afdf6bcf878e6f69794a0dc83fdf1e9a85f7bee3
ekluthra <ekluthra@yahoo.com>
2020-12-27 23:57:21 -0500

    finished homepage by adding background image and name

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,20 @@
 import React from "react"
-
+import image from "../gradient.png";
 export default function Home(){
-    return <h1>Home Page</h1>
+    return (
+        <main>
+            <img 
+                src={image} 
+                alt="gradient" 
+                //changed here
+                className="absolute object-cover w-full h-full z-0"
+            />
+            <section className="relative min-h-screen pt-12 lg:pt-64 px-8">
+                {/* changed here */}
+                <h1 className="text-6xl text-white font-bold navfont leading-none lg:leading-snug">Hi! I'm</h1>
+                <h1 className="text-6xl text-green-100 font-bold navfont leading-none lg:leading-snug home-name">Ekagra Luthra</h1>
+                <h1 className="text-6xl text-white font-bold navfont leading-none lg:leading-snug">and this is my website</h1>
+            </section>
+        </main>
+    )
 }
