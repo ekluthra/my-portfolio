7156a073c932ec9897d45ee11b560bb4f449034f
ekluthra <ekluthra@yahoo.com>
2020-12-30 02:12:36 -0500

    created more responsive layout for text

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,17 +3,17 @@ import image from "../gradient.png";
 export default function Home(){
     return (
         <main>
-            <img 
+            {/* <img 
                 src={image} 
                 alt="gradient" 
-                //changed here
-                className="absolute object-cover w-full h-full z-0"
-            />
-            <section className="relative min-h-screen pt-12 lg:pt-64 px-8">
-                {/* changed here */}
-                <h1 className="text-6xl text-white font-bold navfont leading-none lg:leading-snug">Hi! I'm</h1>
-                <h1 className="text-6xl text-green-100 font-bold navfont leading-none lg:leading-snug home-name">Ekagra Luthra</h1>
-                <h1 className="text-6xl text-white font-bold navfont leading-none lg:leading-snug">and this is my website</h1>
+                className="absolute object-cover w-full h-full"
+            /> */}
+            <section className="bg-gradient-to-l from-gray-700 via-gray-700 to-gray-800 relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-white font-bold navfont leading-none lg:leading-snug">
+                    Hi! I'm 
+                    <h1 className="home-name text-green-100 navfont leading-none lg:leading-snug">Ekagra Luthra</h1>
+                    and this is my website.
+                </h1>
             </section>
         </main>
     )
