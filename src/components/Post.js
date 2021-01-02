b8e0aa56259018002ef9edf41d1119a936d854e0
ekluthra <ekluthra@yahoo.com>
2021-01-01 22:36:54 -0500

    changed colors

--- src/components/Post.js
+++ src/components/Post.js
@@ -23,16 +23,15 @@ export default function Post(){
     }, []);
 
     return(
-        <main className="bg-green-100 min-h-screen p-12">
+        <main className="bg-gradient-to-l from-gray-700 via-gray-700 to-gray-800 min-h-screen p-12">
             <section className="container mx-auto">
-                <h1 className="text-5xl flex justify-center navfont">Blog Posts Page</h1>
-                <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my page of blog posts</h2>
+                <h1 className="text-5xl text-white flex justify-center navfont">Blog Posts Page</h1>
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {postData && postData.map((post, index) => (
                         <article>
                             <Link to={"/post/"+post.slug.current} key={post.slug.current}>
                                 <span 
-                                    className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400" 
+                                    className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-indigo-400" 
                                     key={index}
                                 >
                                     <img 
@@ -41,7 +40,7 @@ export default function Post(){
                                         className="w-full h-full rounded-r object-cover absolute"
                                     />
                                     <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
-                                        <h3 className="text-gray-800 text-lg font-bold px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">
+                                        <h3 className="navfont text-gray-800 text-lg px-3 py-4 bg-blue-700 text-red-100 bg-opacity-75 rounded">
                                             {post.title}
                                         </h3>
                                     </span>
