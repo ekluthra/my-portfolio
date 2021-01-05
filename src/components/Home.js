1bebeefdf3b3e36d10c240cf7089c620c06869a8
ekluthra <ekluthra@yahoo.com>
2021-01-05 00:51:31 -0500

    changed background picture for homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import React from "react"
-import image from "../gradient.png";
+import image from "../tor.jpg";
+import Project from "./Project";
+import About from "./About";
+
 export default function Home(){
     return (
         <main>
@@ -8,13 +11,15 @@ export default function Home(){
                 alt="gradient" 
                 className="absolute object-cover w-full h-full"
             /> */}
-            <section className="bg-gradient-to-l from-gray-700 via-gray-700 to-gray-800 relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-white font-bold navfont leading-none lg:leading-snug">
+            <section className="bg-fixed relative flex justify-center min-h-screen pt-12 lg:pt-32 px-8" style={{backgroundImage: `url(${image})`, backgroundSize: "cover"}}>
+                <h1 className="text-8xl text-white font-bold navfont leading-none lg:leading-snug">
                     Hi! I'm 
-                    <h1 className="home-name text-green-100 navfont leading-none lg:leading-snug">Ekagra Luthra</h1>
+                    <h1 className="text-10xl text text-green-100 navfont leading-none lg:leading-snug">Ekagra Luthra</h1>
                     and this is my website.
                 </h1>
             </section>
+            <About />
+            <Project />
         </main>
     )
 }
