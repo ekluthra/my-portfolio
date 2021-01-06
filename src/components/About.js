1799a392e20819fcc73036f8f5347cc9bf9f9a40
ekluthra <ekluthra@yahoo.com>
2021-01-05 19:18:55 -0500

    template for about page (without block content imported) created

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,33 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client.js";
 
 export default function About(){
-    return <h1>About Page</h1>
+    const [author, setAuthor] = useState(null);
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_type == "author"]{
+            name,
+            bio,
+            "authorImage": image.asset->url
+        }`).then((data) => setAuthor(data[0]))
+        .catch(console.error);
+    }, []);
+
+    if(!author) return <div>Loading...</div>
+
+    return(
+        <main className="relative bg-gradient-to-l from-gray-700 via-gray-700 to-gray-800 min-h-screen p-12">
+            <div>
+                <section>
+                    <img />
+                    <div>
+                        <h1></h1>
+                        <div>
+                            BLOCK CONTENT
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </main>
+    )
 }
