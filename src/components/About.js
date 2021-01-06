639d7139d562c04f5e5446d79b9eee0235fcf2ca
ekluthra <ekluthra@yahoo.com>
2021-01-05 19:40:03 -0500

    changed picture for about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
 import sanityClient from "../client.js";
+import imageUrlBuilder from "@sanity/image-url";
+import pic from "../aboutpic.png";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source){
+    return builder.image(source);
+}
 
 export default function About(){
     const [author, setAuthor] = useState(null);
@@ -16,10 +23,11 @@ export default function About(){
     if(!author) return <div>Loading...</div>
 
     return(
-        <main className="relative bg-gradient-to-l from-gray-700 via-gray-700 to-gray-800 min-h-screen p-12">
-            <div>
-                <section>
-                    <img />
+        <main className="relative bg-gradient-to-l from-gray-700 via-gray-700 to-gray-800 min-h-screen p-6">
+            <div className="p-10 lg:pt-44 container mx-auto relative">
+                <section className="bg-green-100 rounded-lg shadow-2xl lg:flex p-20">
+                    {/* change image later to a more professional one */}
+                    <img src={pic} className="rounded w-32 h-32 lg:w-64 lg:h-64 bg-cover"/>
                     <div>
                         <h1></h1>
                         <div>
